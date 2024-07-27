54e86d1ccba16e2bb4229306cc46eae6882c63ee
dkpanigrahi <dkpanigrahi2001@gmail.com>
2024-07-27 17:05:08 +0530

    Update Some Code

---
+++ src/main/java/com/demo/controller/CustomErrorController.java
@@ -0,0 +1,54 @@
+package com.demo.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CustomErrorController implements ErrorController {
+	
+	@RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            String message;
+
+            switch (statusCode) {
+                case 400:
+                    message = "Bad Request";
+                    break;
+                case 401:
+                    message = "Unauthorized";
+                    break;
+                case 403:
+                    message = "Forbidden";
+                    break;
+                case 404:
+                    message = "Not Found";
+                    break;
+                case 500:
+                    message = "Internal Server Error";
+                    break;
+                default:
+                    message = "Unexpected Error";
+                    break;
+            }
+
+            model.addAttribute("status", statusCode);
+            model.addAttribute("message", message);
+        } else {
+            model.addAttribute("message", "Unexpected Error");
+        }
+        
+        return "error"; // Name of the Thymeleaf template
+    }
+
+    
+    public String getErrorPath() {
+        return "/error";
+    }
+}
